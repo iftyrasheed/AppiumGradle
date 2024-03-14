e0db62695bb65b5a24b1b52f2bd552d90934380c
Aminur Rasheed Ifty <alienifty@gmail.com>
2024-03-14 13:40:32 +0600

    New commit

---
+++ src/test/java/DragNdrop.java
@@ -0,0 +1,30 @@
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DragNdrop extends BaseClass{
+
+    @Test
+    public void dragNdrop() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement source = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+
+        // Java
+        ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) source).getId(),
+                "endX", 618,
+                "endY", 495
+        ));
+
+        Thread.sleep(2000);
+
+        Assert.assertEquals(driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getText(),
+                "Dropped!");
+    }
+}
